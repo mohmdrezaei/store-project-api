4cf2ffa424917ee3fb390a304607f1bf8b33247f
mohmdrezaei <mhwmdrz9@gmail.com>
2025-02-03 17:10:34 +0330

    Creating the initial project structure

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv/config");
+
+app.use(cors());
+app.options("*", cors());
+
+// middleware
+app.use(bodyParser.json());
+
+app.listen(process.env.PORT, () => {
+  console.log(`server is runnig http://localhost:${process.env.PORT}`);
+});
